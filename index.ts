311b9e43d5d974d1bc50037d5cf852e4cd32c8c0
umeradnan7106 <120960218+umeradnan7106@users.noreply.github.com>
2024-07-05 08:26:00 -0700

    save this changes

--- index.ts
+++ index.ts
@@ -2,29 +2,101 @@
 
 import inquirer from "inquirer";
 
-let answer = await inquirer.prompt([
-  { message: "Enter the first number :", type: "number", name: "firstNumber" },
-  { message: "Enter the second number :", type: "number", name: "secondNumber" },
-  {
-    message: "Select the Operator",
-    type: "list",
-    name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-  },
-]);
-
+let answer = await inquirer.prompt(
+    {
+      message: "Select the Operator",
+      type: "list",
+      name: "operator",
+      choices: ["Addition", "Subtraction", "Multiplication", "Division", "Factorial"],
+    },
+);
 
 if(answer.operator === "Addition"){
-    console.log(answer.firstNumber + answer.secondNumber);
+    let answer1 = await inquirer.prompt(
+    [
+        {
+            name: "firstNumber",
+            message: "Enter the first number :",
+            type: "number"
+        },
+        { 
+            name: "secondNumber",
+            message: "Enter the second number :",
+            type: "number", 
+        },
+    ]
+    )
+    console.log(answer1.firstNumber + answer1.secondNumber);
 } 
 else if(answer.operator === "Subtraction"){
-    console.log(answer.firstNumber - answer.secondNumber);
+    let answer1 = await inquirer.prompt(
+        [
+            {
+                name: "firstNumber",
+                message: "Enter the first number :",
+                type: "number"
+            },
+            { 
+                name: "secondNumber",
+                message: "Enter the second number :",
+                type: "number", 
+            },
+        ]
+        )
+    console.log(answer1.firstNumber - answer1.secondNumber);
 } 
 else if(answer.operator === "Multiplication"){
-    console.log(answer.firstNumber * answer.secondNumber);
+    let answer1 = await inquirer.prompt(
+        [
+            {
+                name: "firstNumber",
+                message: "Enter the first number :",
+                type: "number"
+            },
+            { 
+                name: "secondNumber",
+                message: "Enter the second number :",
+                type: "number", 
+            },
+        ]
+        )
+    console.log(answer1.firstNumber * answer1.secondNumber);
 }
 else if(answer.operator === "Division"){
-    console.log(answer.firstNumber / answer.secondNumber);
+    let answer1 = await inquirer.prompt(
+        [
+            {
+                name: "firstNumber",
+                message: "Enter the first number :",
+                type: "number"
+            },
+            { 
+                name: "secondNumber",
+                message: "Enter the second number :",
+                type: "number", 
+            },
+        ]
+        )
+    console.log(answer1.firstNumber / answer1.secondNumber);
+}
+else if(answer.operator === "Factorial"){
+    
+    let factorial = await inquirer.prompt(
+        {
+            name: "fnumber",
+            message: "Enter the factorial:",
+            type: "number"
+        }
+    )
+    
+    function functionOfFactorial(){
+    let result = 1;
+    for(let i = 1; i <= factorial.fnumber; i++){
+          result *= i
+        }
+        console.log(`The Factorial of ${factorial.fnumber}! is ${result} `)
+    }
+    functionOfFactorial()
 }
 else {
     console.log("Select Valid Operator")
